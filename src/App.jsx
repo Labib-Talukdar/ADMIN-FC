4837f06c4e69bde0a875336c6f85624f7b32983e
Labib1851 <labibabeed2007@gmail.com>
2026-07-18 15:13:07 +0600

    rm apcs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+
  
 import './App.css'
 
@@ -7,11 +7,7 @@ function App() {
 
   return (
     <>
-     <div>
-        // "@material-tailwind/react": "^2.1.10",
-    // "@tailwindcss/vite": "^4.3.2",
-    // "@untitledui/icons": "^0.0.22",
-     </div>
+     
     </>
   )
 }
