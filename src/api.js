b39cfbc14cda93152aa4250dddbe15684a7d637e
Labib1851 <labibabeed2007@gmail.com>
2026-07-22 01:14:46 +0600

    add new origin

--- src/api.js
+++ src/api.js
@@ -1,18 +1,4 @@
-// import axios from "axios";
 
-// const API = axios.create({
-//   // Render-এ সেট করা VITE_API_URL নিবে, আর লোকাল ডেভেলপমেন্টের সময় fallback হিসেবে localhost নিবে
-//   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
-// });
-
-// export default API;
-
-
-
-
-
-
- 
 import axios from "axios";
 
 // Vite-এর Environment Variable বা Fallback Localhost URL
